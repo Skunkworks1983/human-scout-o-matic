e71dd7d9a16c03828fe74ffa4b0020e20d2a7362
Westin Miller <equinoxscripts@gmail.com>
2013-10-09 17:31:19 -0700

    Added deferred network actions. Made some of the utilities have their own class.

--- 2013TextUI/src/com/skunk/scoutomatic/textui/net/BackendInterface.java
+++ 2013TextUI/src/com/skunk/scoutomatic/textui/net/BackendInterface.java
@@ -1,34 +1,21 @@
 package com.skunk.scoutomatic.textui.net;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Queue;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.protocol.HTTP;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import android.util.Log;
 
 import com.skunk.scoutomatic.textui.CollectedData;
 import com.skunk.scoutomatic.textui.DataCache;
@@ -39,7 +26,8 @@ import com.skunk.scoutomatic.textui.DataCache;
  * @author "Westin Miller"
  * 
  */
-public class BackendInterface {
+public class BackendInterface implements Runnable {
+	private static final long NETWORK_TRY_SPEED_MILLIS = 30000; // 30 seconds
 	private static final int SCOUT_ID = 0;
 	public static final String EVENT_ID = "2013wase";
 
@@ -47,21 +35,55 @@ public class BackendInterface {
 	private static final String API_MATCHES = "/api/scout/register";
 	private static final String API_POST_MATCH_DATA = "/api/scout/match";
 
+	private static class NetworkAction {
+		private JSONObject postData;
+		private FutureProcessor<String> futureProcessor;
+		private String url;
+		private NameValuePair[] varsGET;
+		private int tries = 0;
+
+		public NetworkAction(String url, JSONObject postData,
+				FutureProcessor<String> callback, NameValuePair[] getVars) {
+			this.url = url;
+			this.postData = postData;
+			this.futureProcessor = callback;
+			this.varsGET = getVars;
+		}
+
+		private boolean tryNetworkAction() {
+			tries++;
+			String s = JSONNetwork.getJSONFromUrl(url, postData, varsGET);
+			if (s == null) {
+				return false;
+			}
+			if (futureProcessor != null) {
+				futureProcessor.run(s);
+			}
+			return true;
+		}
+	}
+
 	private Queue<ScoutableMatch> scoutingQueue = new LinkedBlockingQueue<ScoutableMatch>();
-	private final ExecutorService networkPool;
+	private final ScheduledExecutorService networkPool;
+
+	private List<NetworkAction> deferredNetworkActions = Collections
+			.synchronizedList(new ArrayList<NetworkAction>());
 
 	public BackendInterface() {
-		networkPool = Executors.newFixedThreadPool(1);
+		networkPool = Executors.newScheduledThreadPool(1);
+		networkPool.scheduleAtFixedRate(this, NETWORK_TRY_SPEED_MILLIS,
+				NETWORK_TRY_SPEED_MILLIS, TimeUnit.MILLISECONDS);
 	}
 
 	private void fetchScoutingQueue() {
 		if (scoutingQueue.size() == 0) {
 			// Grab it
 			try {
-				JSONArray thingsToScout = new JSONArray(getJSONFromUrl(
-						API_SERVER + API_MATCHES, null, new BasicNameValuePair(
-								"event_id", EVENT_ID), new BasicNameValuePair(
-								"scout_id", String.valueOf(SCOUT_ID))));
+				JSONArray thingsToScout = new JSONArray(
+						JSONNetwork.getJSONFromUrl(API_SERVER + API_MATCHES,
+								null, new BasicNameValuePair("event_id",
+										EVENT_ID), new BasicNameValuePair(
+										"scout_id", String.valueOf(SCOUT_ID))));
 				for (int i = 0; i < thingsToScout.length(); i++) {
 					scoutingQueue.add(new ScoutableMatch(thingsToScout
 							.getJSONObject(i)));
@@ -90,94 +112,44 @@ public class BackendInterface {
 		});
 	}
 
-	public Future<?> pushMatchInformation(final DataCache cache) {
+	public void pushMatchInformation(final DataCache cache) {
 		try {
 			final JSONObject matchData = CollectedData.createMatchData(cache);
-			return networkPool.submit(new Runnable() {
-				public void run() {
-					try {
-						int mID = matchData.getInt("match_number");
-						fetchScoutingQueue();
-						while (scoutingQueue.size() > 0) {
-							if (scoutingQueue.peek().getMatchID() < mID) {
-								scoutingQueue.poll();
-							} else {
-								break;
+			NetworkAction action = new NetworkAction(API_SERVER
+					+ API_POST_MATCH_DATA, matchData,
+					new FutureProcessor<String>() {
+						@Override
+						public void run(String o) {
+							int mID;
+							try {
+								mID = matchData.getInt("match_number");
+								fetchScoutingQueue();
+								while (scoutingQueue.size() > 0) {
+									if (scoutingQueue.peek().getMatchID() < mID) {
+										scoutingQueue.poll();
+									} else {
+										break;
+									}
+								}
+							} catch (JSONException e) {
 							}
 						}
-						getJSONFromUrl(API_SERVER + API_POST_MATCH_DATA,
-								matchData);
-					} catch (JSONException e) {
-					}
-				}
-			});
+					}, new NameValuePair[0]);
+			deferredNetworkActions.add(action);
+			networkPool.submit(this);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	public static String getJSONFromUrl(String url, JSONObject postData,
-			NameValuePair... getParams) {
-		InputStream is = null;
-		String json = null;
-		try {// .setHeader("Accept", "application/json");
-			DefaultHttpClient httpClient = new DefaultHttpClient();
-			HttpUriRequest httpPost;
-			if (postData == null) {
-				StringBuilder fullURL = new StringBuilder();
-				fullURL.append(url);
-				fullURL.append('?');
-				for (int i = 0; i < getParams.length; i++) {
-					if (i != 0) {
-						fullURL.append('&');
-					}
-					fullURL.append(URLEncoder.encode(getParams[i].getName(),
-							"UTF-8"));
-					fullURL.append('=');
-					fullURL.append(URLEncoder.encode(getParams[i].getValue(),
-							"UTF-8"));
-				}
-				httpPost = new HttpGet(fullURL.toString());
-			} else {
-				httpPost = new HttpPost(url);
-				StringEntity entity = new StringEntity(postData.toString());
-				entity.setContentType("application/json;charset=UTF-8");
-				entity.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE,
-						"application/json;charset=UTF-8"));
-				httpPost.setHeader("Accept", "application/json");
-				((HttpPost) httpPost).setEntity(entity);
+	@Override
+	public void run() {
+		// Try network actions; if we manage remove it
+		Iterator<NetworkAction> iterator = deferredNetworkActions.iterator();
+		while (iterator.hasNext()) {
+			if (iterator.next().tryNetworkAction()) {
+				iterator.remove();
 			}
-			Log.d("NET", httpPost.getURI().toString()
-					+ (postData != null ? " JSON" : ""));
-
-			HttpResponse httpResponse = httpClient.execute(httpPost);
-			HttpEntity httpEntity = httpResponse.getEntity();
-			is = httpEntity.getContent();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return null;
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-			return null;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
 		}
-
-		try {
-			BufferedReader reader = new BufferedReader(
-					new InputStreamReader(is), 8);
-			StringBuilder sb = new StringBuilder();
-			String line = null;
-			while ((line = reader.readLine()) != null) {
-				sb.append(line + "\n");
-			}
-			is.close();
-			json = sb.toString();
-		} catch (Exception e) {
-			Log.e("NET", "Buffer Error converting result " + e.toString());
-			return null;
-		}
-		return json;
 	}
 }
---
+++ 2013TextUI/src/com/skunk/scoutomatic/textui/net/JSONNetwork.java
@@ -0,0 +1,97 @@
+package com.skunk.scoutomatic.textui.net;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+/**
+ * Created on: Oct 9, 2013
+ * 
+ * @author "Westin Miller"
+ * 
+ */
+public class JSONNetwork {
+	public static String getJSONFromUrl(String url, JSONObject postData,
+			NameValuePair... getParams) {
+		InputStream is = null;
+		String json = null;
+		try {// .setHeader("Accept", "application/json");
+			DefaultHttpClient httpClient = new DefaultHttpClient();
+			HttpUriRequest httpPost;
+
+			StringBuilder fullURL = new StringBuilder();
+			fullURL.append(url);
+			fullURL.append('?');
+			for (int i = 0; i < getParams.length; i++) {
+				if (i != 0) {
+					fullURL.append('&');
+				}
+				fullURL.append(URLEncoder.encode(getParams[i].getName(),
+						"UTF-8"));
+				fullURL.append('=');
+				fullURL.append(URLEncoder.encode(getParams[i].getValue(),
+						"UTF-8"));
+			}
+			if (postData == null) {
+				httpPost = new HttpGet(fullURL.toString());
+			} else {
+				httpPost = new HttpPost(fullURL.toString());
+				StringEntity entity = new StringEntity(postData.toString());
+				entity.setContentType("application/json;charset=UTF-8");
+				entity.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE,
+						"application/json;charset=UTF-8"));
+				httpPost.setHeader("Accept", "application/json");
+				((HttpPost) httpPost).setEntity(entity);
+			}
+			Log.d("NET", httpPost.getURI().toString()
+					+ (postData != null ? " JSON" : ""));
+
+			HttpResponse httpResponse = httpClient.execute(httpPost);
+			HttpEntity httpEntity = httpResponse.getEntity();
+			is = httpEntity.getContent();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return null;
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		try {
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(is), 8);
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			is.close();
+			json = sb.toString();
+		} catch (Exception e) {
+			Log.e("NET", "Buffer Error converting result " + e.toString());
+			return null;
+		}
+		return json;
+	}
+}
